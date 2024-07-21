a998d7053d9b509cb92087328289705f16db6a4c
Khoa <khoa.h0086881@gmail.com>
2024-07-22 02:18:16 +0700

    Reservation mockup done

--- react-app/src/components/Cart.jsx
+++ react-app/src/components/Cart.jsx
@@ -1,5 +1,13 @@
 function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <>
+      <div className="container-fluid dark-bg">
+        <div className="container-fluid d-flex justify-content-start ps-5 light-bg">
+          <h3>Cart</h3>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Cart;
--- react-app/src/components/Reservation.jsx
+++ react-app/src/components/Reservation.jsx
@@ -1,4 +1,5 @@
 import ReservationImg from "../assets/reservation-img.jpg";
+import Barista from "../assets/barista.jpg";
 
 function Reservation() {
   return (
@@ -30,7 +31,7 @@ function Reservation() {
       {/* RESERVATION FORM */}
       <div className="container-fluid p-0 m-0 light-bg">
         <div>
-          <section className="h-100 bg-dark">
+          <section className="h-100">
             <div className="container py-5 h-100">
               <div className="row d-flex justify-content-center align-items-center h-100">
                 <div className="col">
@@ -40,123 +41,85 @@ function Reservation() {
                       <div className="col-xl-6">
                         <div className="card-body p-md-5 text-black">
                           <h3 className="mb-5 text-uppercase">
-                            Student registration form
+                            Reservation form
                           </h3>
-                          {/* USERNAME */}
                           <div className="row">
                             <div className="col-md-6 mb-4">
-                              <div data-mdb-input-init className="form-outline">
+                              <div className="form-outline">
                                 <input
                                   type="text"
                                   id="form3Example1m"
-                                  className="form-control form-control-lg"
+                                  className="form-control form-control-lg date-picker"
+                                  placeholder="Name*"
                                 />
-                                <label
-                                  className="form-label"
-                                  htmlFor="form3Example1m"
-                                >
-                                  First name
-                                </label>
                               </div>
                             </div>
-                            <div className="col-md-6 mb-4">
-                              <div data-mdb-input-init className="form-outline">
+                            <div className="col-md-6 mb-1">
+                              <div className="form-outline">
                                 <input
                                   type="text"
-                                  id="form3Example1n"
-                                  className="form-control form-control-lg"
+                                  id="form3Example1m"
+                                  className="form-control form-control-lg date-picker"
+                                  placeholder="Phone*"
                                 />
-                                <label
-                                  className="form-label"
-                                  htmlFor="form3Example1n"
-                                >
-                                  Last name
-                                </label>
                               </div>
                             </div>
                           </div>
                           {/* CONTACT INFORMATION */}
                           <div className="row">
                             <div className="col-md-6 mb-4">
-                              <div data-mdb-input-init className="form-outline">
+                              <div className="form-outline">
                                 <input
-                                  type="text"
+                                  type="date"
                                   id="form3Example1m"
-                                  className="form-control form-control-lg"
+                                  className="form-control form-control-lg date-picker"
                                 />
-                                <label
-                                  className="form-label"
-                                  htmlFor="form3Example1m"
-                                >
-                                  Phone number
-                                </label>
                               </div>
                             </div>
                             <div className="col-md-6 mb-4">
-                              <div data-mdb-input-init className="form-outline">
+                              <div className="form-outline">
                                 <input
                                   type="text"
                                   id="form3Example1n"
-                                  className="form-control form-control-lg"
+                                  className="form-control form-control-lg date-picker"
+                                  placeholder="Time*"
                                 />
-                                <label
-                                  className="form-label"
-                                  htmlFor="form3Example1n"
-                                >
-                                  Email
-                                </label>
                               </div>
                             </div>
                           </div>
                           {/* DATE AND TIME */}
                           <div className="row">
                             <div className="col-md-6 mb-4">
-                              <div data-mdb-input-init className="form-outline">
+                              <div className="form-outline">
                                 <input
-                                  type="date"
+                                  type="number"
                                   id="form3Example1m"
-                                  className="form-control form-control-lg"
+                                  className="form-control form-control-lg date-picker"
+                                  placeholder="Seats*"
                                 />
-                                <label
-                                  className="form-label"
-                                  htmlFor="form3Example1m"
-                                >
-                                  Date
-                                </label>
                               </div>
                             </div>
                             <div className="col-md-6 mb-4">
-                              <div data-mdb-input-init className="form-outline">
+                              <div className="form-outline">
                                 <input
-                                  type="time"
+                                  type="text"
                                   id="form3Example1n"
-                                  className="form-control form-control-lg"
+                                  className="form-control form-control-lg date-picker"
+                                  placeholder="Email"
                                 />
-                                <label
-                                  className="form-label"
-                                  htmlFor="form3Example1n"
-                                >
-                                  Time
-                                </label>
                               </div>
                             </div>
                           </div>
 
                           <div className="row">
                             <div className="col-md-12 mb-4">
-                              <div data-mdb-input-init className="form-outline">
+                              <div className="form-outline">
                                 <textarea
                                   className="form-control"
                                   id="exampleFormControlTextarea1"
-                                  rows="3"
+                                  rows="8"
                                   placeholder="Note*"
                                 ></textarea>
-                                <label
-                                  className="form-label"
-                                  htmlFor="form3Example1m"
-                                >
-                                  Note
-                                </label>
                               </div>
                             </div>
                           </div>
@@ -164,17 +127,19 @@ function Reservation() {
                           <div className="d-flex justify-content-start">
                             <button
                               type="button"
-                              data-mdb-button-init
-                              data-mdb-ripple-init
-                              className="btn btn-warning btn-lg px-5"
+                              className="btn btn-warning px-5"
                             >
                               Submit
                             </button>
                           </div>
                         </div>
                       </div>
-                      <div className="col-xl-6 d-none d-xl-block">
-                        src={ReservationImg}
+                      <div className="col-xl-6 d-none d-xl-block r-form-item">
+                        <img
+                          src={Barista}
+                          alt="barista"
+                          className="w-100 r-form-img"
+                        />
                       </div>
                     </div>
                   </div>
