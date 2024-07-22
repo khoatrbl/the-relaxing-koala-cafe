ab3daee27bad305e7e24e1b08e6ccb287bb437d0
Khoa <khoa.h0086881@gmail.com>
2024-07-22 21:53:43 +0700

    Implementing cart layout

--- react-app/src/components/Cart.jsx
+++ react-app/src/components/Cart.jsx
@@ -1,9 +1,19 @@
 function Cart() {
   return (
     <>
-      <div className="container-fluid dark-bg">
-        <div className="container-fluid d-flex justify-content-start ps-5 light-bg">
-          <h3>Cart</h3>
+      <div className="container-fluid dark-bg px-0">
+        <div className="container ps-5 light-bg">
+          <div className="row pt-4">
+            <div className="col d-flex justify-content-start align-items-end">
+              <h3 className="text-capitalize">Shopping Cart</h3>
+            </div>
+            <div className="col d-flex justify-content-end align-items-end">
+              <h4 className="pe-4">Price</h4>
+            </div>
+            <div>
+              <hr />
+            </div>
+          </div>
         </div>
       </div>
     </>
--- react-app/src/components/Reservation.jsx
+++ react-app/src/components/Reservation.jsx
@@ -2,6 +2,22 @@ import ReservationImg from "../assets/reservation-img.jpg";
 import Barista from "../assets/barista.jpg";
 
 function Reservation() {
+  function handleReservationFormSubmit(e) {
+    e.preventDefault();
+
+    const name = e.target[0].value;
+    const number = e.target[1].value;
+    const date = e.target[2].value;
+    const time = e.target[3].value;
+    const seat = e.target[4].value;
+    const email = e.target[5].value;
+    const note = e.target[6].value;
+
+    alert(
+      `Name: ${name}\nNumber: ${number}\nDate: ${date}\nTime: ${time}\nSeat: ${seat}\nEmail:${email}\nNote: ${note}`
+    );
+  }
+
   return (
     <>
       <div className="container-fluid px-0">
@@ -40,98 +56,108 @@ function Reservation() {
                       {/* FORM SECTION */}
                       <div className="col-xl-6">
                         <div className="card-body p-md-5 text-black">
-                          <h3 className="mb-5 text-uppercase">
-                            Reservation form
-                          </h3>
-                          <div className="row">
-                            <div className="col-md-6 mb-4">
-                              <div className="form-outline">
-                                <input
-                                  type="text"
-                                  id="form3Example1m"
-                                  className="form-control form-control-lg date-picker"
-                                  placeholder="Name*"
-                                />
-                              </div>
-                            </div>
-                            <div className="col-md-6 mb-1">
-                              <div className="form-outline">
-                                <input
-                                  type="text"
-                                  id="form3Example1m"
-                                  className="form-control form-control-lg date-picker"
-                                  placeholder="Phone*"
-                                />
-                              </div>
-                            </div>
-                          </div>
-                          {/* CONTACT INFORMATION */}
-                          <div className="row">
-                            <div className="col-md-6 mb-4">
-                              <div className="form-outline">
-                                <input
-                                  type="date"
-                                  id="form3Example1m"
-                                  className="form-control form-control-lg date-picker"
-                                />
-                              </div>
-                            </div>
-                            <div className="col-md-6 mb-4">
-                              <div className="form-outline">
-                                <input
-                                  type="text"
-                                  id="form3Example1n"
-                                  className="form-control form-control-lg date-picker"
-                                  placeholder="Time*"
-                                />
-                              </div>
-                            </div>
-                          </div>
-                          {/* DATE AND TIME */}
-                          <div className="row">
-                            <div className="col-md-6 mb-4">
-                              <div className="form-outline">
-                                <input
-                                  type="number"
-                                  id="form3Example1m"
-                                  className="form-control form-control-lg date-picker"
-                                  placeholder="Seats*"
-                                />
-                              </div>
-                            </div>
-                            <div className="col-md-6 mb-4">
-                              <div className="form-outline">
-                                <input
-                                  type="text"
-                                  id="form3Example1n"
-                                  className="form-control form-control-lg date-picker"
-                                  placeholder="Email"
-                                />
-                              </div>
-                            </div>
-                          </div>
+                          <form onSubmit={handleReservationFormSubmit}>
+                            <h3 className="mb-5 text-uppercase">
+                              Reservation form
+                            </h3>
 
-                          <div className="row">
-                            <div className="col-md-12 mb-4">
-                              <div className="form-outline">
-                                <textarea
-                                  className="form-control"
-                                  id="exampleFormControlTextarea1"
-                                  rows="8"
-                                  placeholder="Note*"
-                                ></textarea>
+                            <div className="row">
+                              <div className="col-md-6 mb-4">
+                                <div className="form-outline">
+                                  <input
+                                    type="text"
+                                    name="name"
+                                    id="form3Example1m"
+                                    className="form-control form-control-lg date-picker"
+                                    placeholder="Name*"
+                                  />
+                                </div>
+                              </div>
+                              <div className="col-md-6 mb-1">
+                                <div className="form-outline">
+                                  <input
+                                    type="text"
+                                    name="phone"
+                                    id="form3Example1m"
+                                    className="form-control form-control-lg date-picker"
+                                    placeholder="Phone*"
+                                  />
+                                </div>
                               </div>
                             </div>
-                          </div>
+                            {/* CONTACT INFORMATION */}
+                            <div className="row">
+                              <div className="col-md-6 mb-4">
+                                <div className="form-outline">
+                                  <input
+                                    type="date"
+                                    name="date"
+                                    id="form3Example1m"
+                                    className="form-control form-control-lg date-picker"
+                                  />
+                                </div>
+                              </div>
+                              <div className="col-md-6 mb-4">
+                                <div className="form-outline">
+                                  <input
+                                    type="text"
+                                    name="time"
+                                    id="form3Example1n"
+                                    className="form-control form-control-lg date-picker"
+                                    placeholder="Time*"
+                                  />
+                                </div>
+                              </div>
+                            </div>
 
-                          <div className="d-flex justify-content-start">
-                            <button
-                              type="button"
-                              className="btn btn-warning px-5"
-                            >
-                              Submit
-                            </button>
-                          </div>
+                            <div className="row">
+                              <div className="col-md-6 mb-4">
+                                <div className="form-outline">
+                                  <input
+                                    type="number"
+                                    name="seat"
+                                    id="form3Example1m"
+                                    className="form-control form-control-lg date-picker"
+                                    placeholder="Seats*"
+                                  />
+                                </div>
+                              </div>
+                              <div className="col-md-6 mb-4">
+                                <div className="form-outline">
+                                  <input
+                                    type="text"
+                                    name="email"
+                                    id="form3Example1n"
+                                    className="form-control form-control-lg date-picker"
+                                    placeholder="Email"
+                                  />
+                                </div>
+                              </div>
+                            </div>
+
+                            <div className="row">
+                              <div className="col-md-12 mb-4">
+                                <div className="form-outline">
+                                  <textarea
+                                    className="form-control"
+                                    name="note"
+                                    id="exampleFormControlTextarea1"
+                                    rows="8"
+                                    placeholder="Note*"
+                                  ></textarea>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div className="d-flex justify-content-start">
+                              <button
+                                type="submit"
+                                className="btn btn-warning px-5"
+                              >
+                                Submit
+                              </button>
+                            </div>
+                          </form>
                         </div>
                       </div>
                       <div className="col-xl-6 d-none d-xl-block r-form-item">
